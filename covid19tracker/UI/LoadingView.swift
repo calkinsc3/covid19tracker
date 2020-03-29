4346888d284fc54cb23736be9f83abb0e4c830df
Bill Calkins <bill@calkinsconsulting.com>
2020-03-29 13:10:09 -0500

    Added webivew and loading spinner.

---
+++ covid19tracker/UI/LoadingView.swift
@@ -0,0 +1,65 @@
+//
+//  LoadingView.swift
+//  covid19tracker
+//
+//  Created by William Calkins on 3/29/20.
+//  Copyright © 2020 Calkins Computer Consulting. All rights reserved.
+//
+
+import SwiftUI
+
+//MARK:- Activity Indicator
+struct ActivityIndicator: UIViewRepresentable {
+    
+    @Binding var isAnimating : Bool
+    let style : UIActivityIndicatorView.Style
+    
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
+
+//MARK:- Loading View
+struct LoadingView<Content>: View where Content: View {
+    
+    @Binding var isShowing: Bool
+    var loadingStatus: String
+    var content: () -> Content
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .center) {
+                
+                self.content()
+                    .disabled(self.isShowing)
+                    .blur(radius: self.isShowing ? 3 : 0)
+                
+                VStack {
+                    Text(self.loadingStatus)
+                    ActivityIndicator(isAnimating: .constant(true), style: .large)
+                }
+                .frame(width: geometry.size.width / 2, height: geometry.size.height / 5)
+                .background(Color.secondary.colorInvert())
+                .foregroundColor(Color.primary)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .opacity(self.isShowing ? 1 : 0)
+                .animation(.easeInOut)
+            }
+            
+        }
+    }
+}
+
+struct ActivityIndicator_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityIndicator(isAnimating: .constant(true), style: .large)
+    }
+}
+
+
+
--- covid19tracker/UI/StatesWebView.swift
+++ covid19tracker/UI/StatesWebView.swift
@@ -13,12 +13,54 @@ struct StatesWebView: UIViewRepresentable {
     
     let stateInfo: StateInfoElement
     
+    func makeCoordinator() -> StatesWebView.Coordinator {
+        return Coordinator(self)
+    }
+    
     func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView(frame: .zero)
+        webView.uiDelegate = context.coordinator
+        webView.navigationDelegate = context.coordinator
         return WKWebView()
     }
     
     func updateUIView(_ webView: WKWebView, context: Context) {
         webView.load(stateInfo.urlRequest!)
+    }
+    
+    //MARK:- WebDelegate Coordinator
+    class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
+        
+        var parent: StatesWebView
+        
+        init(_ webView: StatesWebView) {
+            self.parent = webView
+            
+        }
+        
+        //MARK:- Navigation Delegate
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            
+//            let alertController = UIAlertController(title: "Done", message: "WebContent did finish", preferredStyle: .alert)
+//            alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//            present(alertController, animated: true)
+            
+        }
+        
+        func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+            
+        }
+        
+        //MARK:- UI Delegate
+        func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+            
+            
+            
+        }
+        
+        
+        
+        
     }
 }
 
